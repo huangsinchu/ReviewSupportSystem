3ce893dd048dae8e31cc890fc196943e261df7d5
Rico1900 <maomao75979@gmail.com>
2016-04-01 14:52:59 +0800

    bug fix again

--- public/components/App.js
+++ public/components/App.js
@@ -113,8 +113,7 @@ var Navbar = React.createClass({
 
  var ReviewPlan = React.createClass({
   edit:function(){
-    var id = "#"+this.props.reviewPlan.id;
-    $(id).modal("toggle");
+    this.props.editPlan(this.props.reviewPlan);
   },
   merge:function(e){
     e.preventDefault();
@@ -245,7 +244,7 @@ var EditModal = React.createClass({
     var url = this.refs.url.value.trim();
     var content = this.refs.content.value.trim();
     var target = this.refs.target.value;
-    var state = $("#state-choose"+id).is(":checked");
+    var state = $(this.refs.switch).is(":checked");
     if(!title||!content){
         return;
     }
@@ -253,24 +252,26 @@ var EditModal = React.createClass({
     var newplan = {"id":id,"title":title,"type":type,"url":url,"content":content,"target":target,"state":state};
     
     this.props.updateReviewPlan(newplan);
-    $("#"+id).modal("toggle");
+
+    $(this.refs.modal).modal("toggle");
   },
   componentDidMount: function() {
-    $('[data-toggle="select"]').select2();
-    $('[data-toggle="switch"]').bootstrapSwitch();
+    $(this.refs.modal).modal('show');
+    $(this.refs.modal).on('hidden.bs.modal', this.props.handleHideModal);
+    $(this.refs.type).select2();
+    $(this.refs.target).select2();
+    $(this.refs.switch).bootstrapSwitch();
   },
 
 	render: function(){
-    var id = this.props.reviewPlan.id;
-    var stateKey = "state-choose"+id;
-    var checkBox = this.props.reviewPlan.state?<input type="checkbox" data-toggle="switch" id={stateKey} checked/>:<input type="checkbox" data-toggle="switch" id={stateKey}/>;
+    var checkBox = this.props.reviewPlan.state?<input type="checkbox" ref="switch" data-toggle="switch" checked/>:<input ref="switch" type="checkbox" data-toggle="switch"/>;
 		var options = [];
     for(var i =0;i < this.props.group.length;i++){
       var temp = <option value={this.props.group[i]}>{this.props.group[i]}</option>;
       options.push(temp);
     }
     return(
-      	<div className="modal fade" id={this.props.reviewPlan.id} tabIndex="-1" role="dialog">
+      	<div className="modal fade" ref="modal" tabIndex="-1" role="dialog">
   			<div className="modal-dialog modal-lg">
    				<div className="modal-content">
               <div className="modal-header">
@@ -287,11 +288,11 @@ var EditModal = React.createClass({
 	      					</div>
 
 	      					<div className="form-group">
-	      						<label for="type-choose" className="col-sm-2 control-label">类型</label>
+	      						<label className="col-sm-2 control-label">类型</label>
 	      						<div className="col-sm-10">
-	      							<select id="type-choose" className="form-control  select select-primary" data-toggle="select" defaultValue={this.props.reviewPlan.type} ref="type">
-                						<option value="文档评审">文档评审</option>
-                						<option value="代码评审">代码评审</option>
+	      							<select className="form-control  select select-primary" data-toggle="select" defaultValue={this.props.reviewPlan.type} ref="type">
+                				<option value="文档评审">文档评审</option>
+                				<option value="代码评审">代码评审</option>
               				</select>
 	      						</div>
 	      					</div>
@@ -315,7 +316,7 @@ var EditModal = React.createClass({
 	      					<div className="form-group">
 	      						<label for="target-choose" className="col-sm-2 control-label">目标</label>
 	      						<div className="col-sm-10">
-	      							<select className="form-control select select-primary" id="target-choose" ref="target" defaultValue={this.props.reviewPlan.target} data-toggle="select" ref="target">
+	      							<select className="form-control select select-primary" ref="target" defaultValue={this.props.reviewPlan.target} data-toggle="select">
 	                		 {options}			
               				</select>
 	      						</div>
@@ -373,7 +374,9 @@ var ReviewList = React.createClass({
            "target":"文档评审组",
            "content":"论文内容包含对中国dota的局势分析，请仔细评审。"
           }
-          ]
+          ],
+          showModal:false,
+          needEditPlan:{}
       };
   },
   loadReviewsFromServer:function(){
@@ -395,12 +398,21 @@ var ReviewList = React.createClass({
   },
   addReviewPlan:function(newplan){
     var reviewPlans = this.state.reviewPlanList;
-    newplan.id="1199";
-    var newList = reviewPlans.concat([newplan]);
-    this.setState({reviewPlanList:newList});
+    var newList = [newplan].concat(reviewPlans);
+    
     //TODO:commit to server
+    this.setState({reviewPlanList:newList});
   },
-
+  handleHideModal:function(){
+    this.setState({showModal:false});
+  },
+  handleShowModal:function(){
+    this.setState({showModal:true});
+  },
+  editPlan:function(review){
+    this.setState({needEditPlan:review});
+    this.handleShowModal();
+  },
   updateReviewPlan:function(newplan){
         var oldList = this.state.reviewPlanList;
         var reviewPlans = [].concat(oldList);
@@ -415,16 +427,10 @@ var ReviewList = React.createClass({
 
   
   render:function(){
-      var ReviewPlans = this.state.reviewPlanList.map(function(plan){
-          return(
-            <ReviewPlan reviewPlan={plan}/>
-          );
-      });
-
-      var EditModals = [];
-      for(var i =0;i < this.state.reviewPlanList.length;i++){
-        var temp = <EditModal reviewPlan={this.state.reviewPlanList[i]} updateReviewPlan={this.updateReviewPlan} group={this.props.profile.group}/>;
-        EditModals.push(temp);
+      var ReviewPlans = [];
+      for(var i = 0;i < this.state.reviewPlanList.length;i++){
+        var temp = <ReviewPlan reviewPlan={this.state.reviewPlanList[i]} editPlan={this.editPlan} />
+        ReviewPlans.push(temp);
       }
 
       return(
@@ -433,7 +439,8 @@ var ReviewList = React.createClass({
           <div className="row">
             {ReviewPlans}          
           </div>
-          {EditModals}
+          {this.state.showModal?<EditModal reviewPlan={this.state.needEditPlan}
+          handleHideModal={this.handleHideModal} updateReviewPlan={this.updateReviewPlan} group={this.props.profile.group}/>:null}
         </div>
       );
   }
